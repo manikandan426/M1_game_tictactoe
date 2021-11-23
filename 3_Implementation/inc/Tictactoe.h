978156c5c1c5b743877dd6178d1a23d280284535
manikandan426 <71758695+manikandan426@users.noreply.github.com>
2021-11-23 21:12:04 +0530

    Update Tictactoe.h

--- 3_Implementation/inc/Tictactoe.h
+++ 3_Implementation/inc/Tictactoe.h
@@ -1,1 +1,19 @@
+# ifndef TICTACTOE_DOT_H
+#define  TICTACTOE_DOT_H
+/**
+ * @file Tictactoe.h
+ * @author sahithidommeti ()
+ * @brief  creating headers
+ * @version 0.1
+ * @date 2021-09-07
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+#include<stdio.h>
+#include <stdlib.h>
+int checkwin(char *);
+void board(char *);
+int main();
 
+#endif
