a294471aba20720cc6edf8ed022fb7536c993296
manikandan426 <71758695+manikandan426@users.noreply.github.com>
2021-11-23 21:15:49 +0530

    Update board.c

--- 3_Implementation/src/board.c
+++ 3_Implementation/src/board.c
@@ -1,1 +1,35 @@
+#include<stdio.h>
+ #include<Tictactoe.h>
+ void board(char square[])
+{
+    //char square[10] = { 'o', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+    
 
+    printf("cls");
+    printf("\n\n   \xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\n");
+    printf("\t   Tic -Tac-Toe");
+    printf("\n\n   \xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\n");
+ 
+    printf("\t  *****Start Game*****");
+    printf("\n   \xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\n\n");
+ 
+    printf("   Player 1 (X)  -  Player 2 (O)\n\n");
+ 
+ 
+    printf("   \xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\n");
+    printf("\t|     |     |     |\n");
+    printf("\t|  %c  |  %c  |  %c  |\n", square[1], square[2], square[3]);
+ 
+    printf("\t|_____|_____|_____|\n");
+    printf("\t|     |     |     |\n");
+ 
+    printf("\t|  %c  |  %c  |  %c  |\n", square[4], square[5], square[6]);
+ 
+    printf("\t|_____|_____|_____|\n");
+    printf("\t|     |     |     |\n");
+ 
+    printf("\t|  %c  |  %c  |  %c  |\n", square[7], square[8], square[9]);
+ 
+    printf("\t|     |     |     |\n");
+    printf("   \xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\n\n");
+}
